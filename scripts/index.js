962f8a2cc3cf6e0b415f5e1a49a0b58c7b870172
fernanda gradilla <f.gradilla.06@outlook.com>
2025-06-17 10:06:43 -0600

    se modifica boton

--- scripts/index.js
+++ scripts/index.js
@@ -5,17 +5,47 @@ const displayDescription = document.querySelector(
   ".profile__details-description"
 );
 //Botones de abrir y cerrar popups
+
+//registrar un evento de click a un button (puedes registrar a cualquier cosa, imagenes, pararrafos, etc)
+const contentInfoEditButton = document.querySelector(
+  ".content__info_edit_button"
+); //seleccionar
+
+contentInfoEditButton.addEventListener("click", () => {
+  console.log("Hola FernahandlePopupOpen(nda");
+  handlePopupOpen();
+}); //registrar el click
+function handlePopupOpen() {
+  const openPopup = document.querySelector("#popup-add");
+  openPopup.classList.remove("popup_opened");
+}
+
+ ButtonCancel.addEventListener('click',() => {});
+console.log("Hola FernahandlePopupCosed(nda");
+function handlePopupClose()
+const closedPopup = document.querySelector("#popup-add");
+closedPopup.classList.add("popup_closed"); {
+    
+  
+
+
+   
+ 
+}
+//1.-tener una variable con que identifique el codigo html 
+//2.-agregarle la posibilidad de hacerle un click
+//3.-En el click llamar a la funcion para cerrar el popup
+
 const openPopupButton = document.querySelector(".profile__info-button");
 const openPopupAddButton = document.querySelector(".profile__info-button-add");
 const closePopupButton = document.querySelector(".popup__button-close");
 const closePopupAddButton = document.querySelector(".popup-add__button-close");
 const closePopupImageButton = document.querySelector(
   ".popup-image__button-close"
- 
 );
 //Popups
 const popupElement = document.querySelector(".popup");
-const popupAddElement = document.querySelector("#popup-add");
+
 const popupImageElement = document.querySelector(".popup-image");
 
 //Popup content
@@ -57,10 +87,6 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
-
-function handlePopupOpen(openPopup) {
-  openPopup.classList.add("popup_opened");
-}
 
 function handlePopupClose() {
   popupElement.classList.remove("popup_opened");
